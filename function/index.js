29bb9428b56750fb967e09f44b4cea32806324bc
João Pedro Nogueira <54480518+Hanter-byte@users.noreply.github.com>
2023-06-11 14:32:30 -0300

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,11 +1,19 @@
 const handler = async (event) => {
     const word = event.queryStringParameters && event.queryStringParameters.word ? event.queryStringParameters.word : '';
     const letterCount = word.length;
+    let response;
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(`A palavra "${word}" possui ${letterCount} caracteres.`),
-    };
+    if (letterCount === 1) {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(`A palavra "${word}" possui ${letterCount} caracter.`),
+        };
+    } else {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(`A palavra "${word}" possui ${letterCount} caracteres.`),
+        };
+    }
 
     return response;
 };
